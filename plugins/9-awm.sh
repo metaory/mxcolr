368e61c6e4e49e7446b110028f4be782b413043e
metaory <metaory@gmail.com>
2021-05-16 22:01:53 +0800

    awm cleanup and tests

--- plugins/9-awm.sh
+++ plugins/9-awm.sh
@@ -1,59 +1,67 @@
 #!/usr/bin/env bash
+BS="$(GetPlugName)"
+
+AWESOME_THEME=${AWESOME_THEME:-$USER}
+AWESOME_THEME_PATH="$XDG_CONFIG_HOME"/awesome/themes/"$AWESOME_THEME"
 
 ################################
-notify () { awesome-client "require('naughty').notify({ bg='$WBG', fg='$WFG',  timeout=3, opacity = 0.8, text='${1}'})" 2>/dev/null; }
+notify () { 
+  if command -v awesome-client &> /dev/null; then
+    awesome-client "require('naughty').notify({ bg='$WBG', fg='$WFG',  timeout=3, opacity = 0.8, text='${1}'})" 2>/dev/null
+  else
+    echo -e "\n ==> $* \n"
+  fi
+}
 ################################
 ApplyWallpaper () {
   PromptContinue; if [[ ! "$REPLY" =~ ^[Yy]$ ]]; then return; fi
   local stamp; stamp=$(date +%s); stamp="${stamp:(-8)}"
   local base="$HOME"/pics/wall/BASE.png
-  if ! [ -e "$base" ]; then Info 1 "base wallpaper not found"; Info 1 "$base"; exit 1; fi
+  if ! [ -e "$base" ]; then Info 1 "base wallpaper not found"; Info 1 "$base"; return; fi
+  cp -v "$HOME"/pics/wall/curr.png "$HOME"/pics/wall/hist/"${stamp}".png
   convert "$HOME"/pics/wall/BASE.png -fill "${WBG}" -tint 100%  "$HOME"/pics/wall/curr.png
-  # convert "$HOME"/pics/wall/curr.png -fill "${WBG}" -tint 100%  "$HOME"/pics/wall/curr.png
-  cp "$HOME"/pics/wall/curr.png "$HOME"/pics/wall/hist/"${stamp}".png
-  InfoDone
+  InfoDone 
 }
 # ////////////////////////////  
 ################################
 ApplyIcons () {
   if [[ "$XOPT" == *"noico"* ]]; then InfoIgnore; return; fi
   PromptContinue; if [[ ! "$REPLY" =~ ^[Yy]$ ]]; then return; fi
+  # shellcheck disable=SC2046
+  mapfile -t TARGET_ICONS <<<$(find "$AWESOME_THEME_PATH"/icons/{layout,apps} -name "*.png" -type f)
 
-  local LAYOUT_ICONS=("$XDG_CONFIG_HOME"/awesome/themes/metaory/icons/layout/*.png)
-  info "LAYOUT_ICONS-Count: ${LAYOUT_ICONS[*]}"
-  for layoutico in "${LAYOUT_ICONS[@]}"; do
-    convert "$layoutico" -fill "${DL6}" -colorize 100%  "$layoutico"
-  done
-  info "━  ━━ updated ${#LAYOUT_ICONS[@]} layout icons" 0
+  local steam_tray_icon=/usr/share/pixmaps/steam_tray_mono.png
+  [ -w "$steam_tray_icon" ] && TARGET_ICONS+=("$steam_tray_icon")
 
-  info "━  ━ app icons" 2
-  # zz="$(find  icons/apps/*.png -print)"; for appico in "${zz[@]}"; do echo "====  $appico"; done
-  local APP_ICONS=("$XDG_CONFIG_HOME"/awesome/themes/metaory/icons/apps/*.png)
-  info "APP_ICONS-Count: ${APP_ICONS[*]}" 2
-  for appico in "${APP_ICONS[@]}"; do
-    convert "$appico" -fill "${DL6}" -colorize 100%  "$appico"
+  info "${TARGET_ICONS[*]} TARGET_ICONS"
+
+  for ico in "${TARGET_ICONS[@]}"; do
+    cp -v "$ico" /tmp/mxc/"${BS}_${ico##*/}"
+    convert "$ico" -fill "${DL6}" -colorize 100%  "$ico"; # < < TODO mv to tmp first
   done
-  Info "━  ━━ updated ${#APP_ICONS[@]} app icons" 0
-  # sudo cp ~mtheme/icons/steam/steam_tray_mono.png /usr/share/pixmaps/steam_tray_mono.png
-  # cp /usr/share/pixmaps/steam_tray_mono.png /tmp/mx__steam_tray_mono.png
-  convert /usr/share/pixmaps/steam_tray_mono.png -fill "${DL6}" -colorize 100%  /usr/share/pixmaps/steam_tray_mono.png
-  Info "" 0
+
+  InfoDone "${TARGET_ICONS[*]} TARGET_ICONS"
+
 }
 # ////////////////////////////  
 # ////////////////////////////  
+
 RestartAWM () {
   PromptContinue; if [[ ! "$REPLY" =~ ^[Yy]$ ]]; then return; fi
   notify "restarting AWM in 3s ⏲" ; sleep 3
-  (awesome-client 'awesome.restart()' &>/dev/null || true)
-  # awesome --replace & disown
+
+  if command -v awesome-client &> /dev/null; then
+    awesome-client 'awesome.restart()' &>/dev/null || true
+  fi
+
+    # awesome --replace & disown
   killall steam &>/dev/null
   killall nm-applet &>/dev/null
   nm-applet & disown
-  Info "" 0
+  InfoDone
 }
 
 apply_awm () {
-  if [[ "$XOPT" == *"noawm"* ]]; then InfoIgnore; return; fi
   ApplyWallpaper
   ApplyIcons
   RestartAWM
