9ffd3ed2159fe3e786daa2959059cda1f7aee78b
metaory <metaory@gmail.com>
2022-01-02 09:09:08 +0800

    refactor shade names

--- templates/mxc-nvim-base16.lua
+++ templates/mxc-nvim-base16.lua
@@ -1,6 +1,6 @@
 local base16 = {
-  base00="${XBG}"; base01="${DK3}"; base02="${DL4}"; base03="${LK5}";
-  base04="${LK6}"; base05="${DL7}"; base06="${DK8}"; base07="${DL9}";
+  base00="${XBG}"; base01="${SK3}"; base02="${WK4}"; base03="${EK5}";
+  base04="${EK6}"; base05="${WK7}"; base06="${SK8}"; base07="${WK9}";
   base08="${C01}"; base09="${C03}"; base0A="${C03}"; base0B="${C05}";
   base0C="${C02}"; base0D="${C04}"; base0E="${WBG}"; base0F="${SBG}";
 }
