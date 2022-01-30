583aba6fac38282614c24d5066fe70d919ddb8a2
metaory <metaory@gmail.com>
2022-01-30 13:45:54 +0800

    tmux plugin bug fixes

--- plugins/2-tmux.sh
+++ plugins/2-tmux.sh
@@ -1,31 +1,25 @@
 #!/usr/bin/env bash
 # #################
 
-BS="$(GetPlugName).mx"
-MXC_TMUX_TMP=/tmp/mxc/"$BS"
-MXC_TMUX_OUT="${MXC_TMUX_OUT:-"$MXDIST/$BS"}"
+MXC_TMUX_TMP=/tmp/mxc/tmux.mx
+MXC_TMUX_OUT="${MXC_DEFAULT_TMUX_OUT:-$HOME/.config/tmux/tmux.mx}"
+M_THEME=$HOME/.config/tmux/meta.min.tmuxtheme
 
 # shellcheck disable=SC2046
 PopulateFileWith "$MXC_TMUX_TMP" 'FLUSH' \
   "\${c}=\'\${!c}\'" \
   "${MX_VARS[@]}"
-  # $(eval echo \$\{MX_Z{C,X,M,K,L}\[\@\]\})
-
-# since https://github.com/metaory/mxcolr/commit/f6d25e
-# most of this file couple be replaced by a simple template file
 
 Info "$MXC_TMUX_TMP"
 
 [ "$TMUX" ] && tmux run-shell "tmux source-file $MXC_TMUX_TMP"
-[ "$TMUX" ] && [ "$M_THEME" ] && tmux run-shell "tmux source-file $M_THEME"
+[ "$TMUX" ] && tmux run-shell "tmux source-file $M_THEME"
 
 apply_tmux () {
   local TSOCK=''; # KSOCK='';KFLAG='';  # KSOCK='--to unix:/tmp/kitty-mtx' # KFLAG='-a -c'
   if [ "$TMUX" ]; then TSOCK="$(tmux display -p "#{b:socket_path}")"; else TSOCK="${MX_DEFAULT_TMUX_SOCKET:-master}"; fi
 
   cp -v "$MXC_TMUX_TMP" "$MXC_TMUX_OUT"
-  # [ "$1" ] || ! [ -e "$MXC_TMUX_OUT" ] && cp -v "$MXC_TMUX_TMP" "$MXC_TMUX_OUT"
-  # ! [ -e "MXC_TMUX_OUT" ] &&
 
   tmux -L "$TSOCK"  run-shell "tmux source-file $MXC_TMUX_OUT"
   [ "$M_THEME" ] && tmux -L "$TSOCK" run-shell "tmux source-file $M_THEME"
